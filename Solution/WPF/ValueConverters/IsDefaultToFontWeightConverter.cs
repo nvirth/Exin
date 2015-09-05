d98a47487a616f615ef1819bf5855cd9d865effc
Norbert Virth <norbert.virth@gmail.com>
2015-09-05 11:11:18 +0200

    Default button bold + add-new-item labels' width
    * The default button is bold
    * The Add-new-daily-expense and Add-new-montly-income forms' labels width is fixed (to Grid auto)
    * Exin issues.xlsx: Marked some already finished tasks

---
+++ Solution/WPF/ValueConverters/IsDefaultToFontWeightConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using Common.Db.Entities;
+using DAL.DataBase.Managers;
+using DAL.RepoCommon.Managers;
+
+namespace WPF.ValueConverters
+{
+	public class IsDefaultToFontWeightConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var boolValue = value as bool?;
+			if(boolValue.HasValue && boolValue.Value)
+				return FontWeights.Bold;
+
+			return FontWeights.Normal;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
